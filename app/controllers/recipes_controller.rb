95c65a381a780173de33ca948de12110916c023e
vita <dragic.hsieh@gmail.com>
2018-06-02 19:23:34 +0800

    Done recipes controller action and create model

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,51 @@
 class RecipesController < ApplicationController
+
+  before_action :find_recipe, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @recipes = Recipe.order("created_at DESC")
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe, notice: "美味食譜成功建立"
+    else
+      render "new"
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      redirect_to @recipe, notice: "美味食譜修改成功"
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to recipes_path, notice: "美味食譜被刪除了"
+  end
+
+  private
+
+  def find_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :description)
+  end
+
 end
