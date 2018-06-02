b3e58ab039cc54d2e2bbc6e7029b518e894203e0
vita <dragic.hsieh@gmail.com>
2018-06-03 00:35:50 +0800

    Done paperclip gem then i can upload image

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -45,7 +45,7 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:title, :description)
+    params.require(:recipe).permit(:title, :description, :image)
   end
 
 end
